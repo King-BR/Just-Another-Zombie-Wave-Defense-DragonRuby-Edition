b2caa08452d648ed5727863de05c9f28d8a4494e
Fernando <fernandopach2017@gmail.com>
2021-08-26 20:09:38 -0300

    up

--- app/main.rb
+++ app/main.rb
@@ -12,7 +12,10 @@ class Enemy
   end
 
   def pursuePlayer player_x, player_y
-    if !@args.geometry.point_inside_circle? [@x, @y], [player_x, @player_y], @entity_size
+    dis = @args.geometry.distance [@x, @y], [player_x, player_y];
+
+    if (dis > @entity_size) 
+
       if @x < player_x
         @x += @speed;
       elsif @x > player_x
@@ -28,6 +31,9 @@ class Enemy
   end
 
   def render
+    #@args.outputs.solids << [@x - @entity_size/2, @y - @entity_size/2, @entity_size, @entity_size, 0, 255, 0, 255];
+    #@args.outputs.solids << [@x, @y, 1, 1, 255, 0, 0, 255];
+    
     @args.outputs.sprites << {
       x: @x - @entity_size/2,
       y: @y - @entity_size/2, 
@@ -61,6 +67,7 @@ class Game
 
   def handlePlayerMovement
     if @args.inputs.up
+      #@args.geometry.distance [@player_x, @player_y],
       @player_y += 5;
     end
     
@@ -78,18 +85,24 @@ class Game
   end
 
   def render_player
+    #@args.outputs.solids << [@player_x - @entity_size/2, @player_y - @entity_size/2, @entity_size, @entity_size, 0, 0, 255, 255];
+    #@args.outputs.solids << [@player_x, @player_y, 1, 1, 255, 0, 0, 255];
+
     @args.outputs.sprites << {
       x: @player_x - @entity_size/2,
-      y: @player_y - @entity_size/2,
+      y: @player_y - @entity_size/2, 
       w: @entity_size, 
-      h: @entity_size,
+      h: @entity_size, 
       path: 'sprites/circle/blue.png'
     };
     
     @args.outputs.labels << {
       x: @player_x - 12 - @entity_size/2,
       y: @player_y - 5 - @entity_size/2,
-      text: "#{@player_hp}/#{@player_maxhp}"
+      text: "#{@player_hp}/#{@player_maxhp}",
+      r: 255,
+      g: 0,
+      b: 0
     };
   end
 
