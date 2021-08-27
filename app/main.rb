7f529d95a24e2ce4cfb5a6ec3a17a94dbd75cfd4
Fernando <fernandopach2017@gmail.com>
2021-08-26 21:27:57 -0300

    up

--- app/main.rb
+++ app/main.rb
@@ -1,20 +1,5 @@
 $gtk.reset
 
-colors = {
-  RED = [255, 0, 0, 255],
-  GREEN = [0, 255, 0, 255],
-  BLUE = [0, 0, 255, 255],
-  CYAN = [0, 255, 255, 255],
-  PINK = [255, 0, 255, 255],
-  YELLOW = [255, 255, 0, 255],
-  ORANGE = [255, 150, 0, 255]
-  BLACK = [0, 0, 0, 255],
-  WHITE = [255, 255, 255, 255],
-  GRAY = [128, 128, 128, 255],
-  DARK_GRAY = [90, 90, 90, 255],
-  LIGHT_GRAY = [190, 190, 190, 255]
-};
-
 class Enemy
   def initialize args
     @args = args
@@ -46,8 +31,9 @@ class Enemy
   end
 
   def render
-    #@args.outputs.solids << [@x - @entity_size/2, @y - @entity_size/2, @entity_size, @entity_size, 0, 255, 0, 255];
-    #@args.outputs.solids << [@x, @y, 1, 1, 255, 0, 0, 255];
+    # Debug sprite position
+    #@args.outputs.solids << [@x - @entity_size/2, @y - @entity_size/2, @entity_size, @entity_size, *@args.state.colors.GREEN];
+    #@args.outputs.solids << [@x, @y, 1, 1, *@args.state.colors.RED];
     
     @args.outputs.sprites << {
       x: @x - @entity_size/2,
@@ -111,13 +97,15 @@ class Game
       path: 'sprites/circle/blue.png'
     };
     
+    r, g, b, a = @args.state.colors.RED
     @args.outputs.labels << {
       x: @player_x - 12 - @entity_size/2,
       y: @player_y - 5 - @entity_size/2,
       text: "#{@player_hp}/#{@player_maxhp}",
-      r: 255,
-      g: 0,
-      b: 0
+      r: r,
+      g: g,
+      b: b,
+      a: a
     };
   end
 
@@ -142,6 +130,22 @@ end
 
 def tick args
   $gtk.suppress_mailbox = false;
+
+  args.state.colors = {
+    RED: [255, 0, 0, 255],
+    GREEN: [0, 255, 0, 255],
+    BLUE: [0, 0, 255, 255],
+    CYAN: [0, 255, 255, 255],
+    PINK: [255, 0, 255, 255],
+    YELLOW: [255, 255, 0, 255],
+    ORANGE: [255, 150, 0, 255],
+    BLACK: [0, 0, 0, 255],
+    WHITE: [255, 255, 255, 255],
+    GRAY: [128, 128, 128, 255],
+    DARK_GRAY: [90, 90, 90, 255],
+    LIGHT_GRAY: [190, 190, 190, 255]
+  };
+  
 
   args.state.entity_size ||= 40;
   args.state.game ||= Game.new args;
